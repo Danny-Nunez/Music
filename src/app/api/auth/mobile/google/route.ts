ad59914b65959e4efccb2d4ed0e77cdc58a31fc3
Danny Nunez <dnunez22@gmail.com>
2025-02-26 19:34:24 -0500

    updates

--- src/app/api/auth/mobile/google/route.ts
+++ src/app/api/auth/mobile/google/route.ts
@@ -4,7 +4,7 @@ import crypto from 'crypto';
 import { google } from 'googleapis';
 
 const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_IOS_CLIENT_ID,
+  process.env.GOOGLE_EXPO_CLIENT_ID,
   process.env.GOOGLE_EXPO_CLIENT_SECRET,
   undefined  // No redirect URI needed for mobile flow
 );
